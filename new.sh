632a4a8aedc255305be780bd1382e5babb9fea64
Abhinav <32412878+k40t1k-sys@users.noreply.github.com>
2022-06-24 15:17:29 +0530

    added script

---
+++ new.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+touch $1.cpp
+
+echo '#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen(“input.txt”, “r”, stdin);
+	freopen(“output.txt”, “w”, stdout);
+	#endif
+	
+	return 0;
+}' > $1.cpp
