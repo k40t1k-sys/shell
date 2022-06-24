67d42c480e32fb01ac78a2e6fb21ae37fdf4c42d
Abhinav <32412878+k40t1k-sys@users.noreply.github.com>
2022-06-24 20:22:44 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,9 +8,11 @@ using namespace std;
 
 int main() {
 	#ifndef ONLINE_JUDGE
-	freopen(“input.txt”, “r”, stdin);
-	freopen(“output.txt”, “w”, stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 	#endif
 	
 	return 0;
 }' > $1.cpp
+
+subl $1.cpp
